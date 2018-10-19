5c202a639a1fcfca4a4a2053cbf21242216b57a5
jackjamieson2 <jackjamieson@gmail.com>
2018-10-19 14:20:52 -0400

    Switch from file_get_contents to wp_remote_get

--- includes/class-microsub-parser.php
+++ includes/class-microsub-parser.php
@@ -5,9 +5,10 @@
  * @author Jack Jamieson
  *
  */
+
 class Yarns_Microsub_Parser {
-
-
+	
+	
 	/**
 	 * Final clean up on post content before saving.
 	 *
@@ -32,7 +33,7 @@ class Yarns_Microsub_Parser {
 				}
 			}
 		}
-
+		
 		// Attempt to set a featured image
 		if ( ! isset( $data['featured'] ) ) {
 			if ( isset( $data['photo'] ) && is_array( $data['photo'] ) && 1 === count( $data['photo'] ) ) {
@@ -40,23 +41,23 @@ class Yarns_Microsub_Parser {
 				unset( $data['photo'] );
 			}
 		}
-
+		
 		// Convert special characters to html entities in content['html']
 		/*
 		if ( isset( $data['content']['html'] ) ) {
 			//$data['content']['html'] = htmlspecialchars( $data['content']['html']);
 		}
 		*/
-
-
+		
+		
 		// Some feeds return multiple author photos, but only one can be shown
 		if ( isset( $data['author']['photo'] ) ) {
 			if ( is_array( $data['author']['photo'] ) ) {
 				$data['author']['photo'] = $data['author']['photo'][0];
 			}
 		}
-
-
+		
+		
 		//debugging
 		$ref_types = [ 'like-of', 'repost-of', 'bookmark-of', 'in-reply-to' ];
 		// When these types contain an array (name, url, type) it causes together to crash - see https://github.com/cleverdevil/together/issues/80
@@ -66,21 +67,21 @@ class Yarns_Microsub_Parser {
 				$data[ $ref_type ] = $data[ $ref_type ]['url'];
 			}
 		}
-
+		
 		// referecnes
-
+		
 		if ( isset( $data['in-reply-to']['url'] ) ) {
-
+			
 			//$data['in-reply-to'] = $data['in-reply-to']['url'];
 			//unset($data['in-reply-to']);
 		}
-
-
+		
+		
 		$data = encode_array( array_filter( $data ) );
-
+		
 		return $data;
 	}
-
+	
 	/**
 	 * Parses marked up HTML.
 	 *
@@ -91,13 +92,13 @@ class Yarns_Microsub_Parser {
 	 */
 	public static function mergeparse( $content, $url ) {
 		// For debugging - get time of the script
-
+		
 		if ( empty( $content ) || empty( $url ) ) {
 			return array();
 		}
-
+		
 		$mf2data = Parse_This_MF2::mf2parse( $content, $url );
-
+		
 		$data = $mf2data;
 		if ( ! isset( $data['summary'] ) && isset( $data['content'] ) ) {
 			$data['summary'] = substr( $data['content']['text'], 0, 300 );
@@ -119,71 +120,84 @@ class Yarns_Microsub_Parser {
 				unset( $data['photo'] );
 			}
 		}
-
+		
 		// Convert special characters to html entities in content['html']
 		if ( isset( $data['content']['html'] ) ) {
 			$data['content']['html'] = htmlspecialchars( $data['content']['html'] );
 		}
-
+		
 		return $data;
-
+		
 	}
-
-
-
+	
+	
+	
 	/*Search
 
 	action=search
 	query = {URI to search}*/
+	
+	/*HTTP/1.1 200 Ok
+Content-type: application/json
 
-				/*HTTP/1.1 200 Ok
-			Content-type: application/json
-
-			{
-			  "results": [
-				{
-				  "type": "feed",
-				  "url": "https://aaronparecki.com/",
-				  "name": "Aaron Parecki",
-				  "photo": "https://aaronparecki.com/images/profile.jpg",
-				  "description": "Aaron Parecki's home page"
-				},
-				{
-				  "type": "feed",
-				  "url": "https://percolator.today/",
-				  "name": "Percolator",
-				  "photo": "https://percolator.today/images/cover.jpg",
-				  "description": "A Microcast by Aaron Parecki",
-				  "author": {
-					"name": "Aaron Parecki",
-					"url": "https://aaronparecki.com/",
-					"photo": "https://aaronparecki.com/images/profile.jpg"
-				  }
-				},
-				{ ... }
-			  ]
-			}*/
-
-
+{
+  "results": [
+	{
+	  "type": "feed",
+	  "url": "https://aaronparecki.com/",
+	  "name": "Aaron Parecki",
+	  "photo": "https://aaronparecki.com/images/profile.jpg",
+	  "description": "Aaron Parecki's home page"
+	},
+	{
+	  "type": "feed",
+	  "url": "https://percolator.today/",
+	  "name": "Percolator",
+	  "photo": "https://percolator.today/images/cover.jpg",
+	  "description": "A Microcast by Aaron Parecki",
+	  "author": {
+		"name": "Aaron Parecki",
+		"url": "https://aaronparecki.com/",
+		"photo": "https://aaronparecki.com/images/profile.jpg"
+	  }
+	},
+	{ ... }
+  ]
+}*/
+	
+	
 	/**
 	 * Searches a URL for feeds
+	 *
 	 * @param $query
 	 *
 	 * @return array|void
+	 * //@@todo Check if the content itself is an rss feed and if so just return that
 	 */
 	public static function search( $query ) {
-
+		
 		// Check if $query is a valid URL, if not try to generate one
-		$url     = static::validate_url( $query );
-		$content = file_get_contents( $url ); //get the html returned from the following url
-		$dom     = new DOMDocument();
+		$url    = static::validate_url( $query );
+		$remote = wp_remote_get( $url );
+		
+		if ( is_wp_error( $remote ) ) {
+			return "Error accessing {$url}";
+		}
+		
+		
+		$content = $remote['body'];
+		
+		
+		$dom = new DOMDocument();
 		libxml_use_internal_errors( true ); //disable libxml errors
 		if ( ! empty( $content ) ) { //if any html is actually returned
 			$dom->loadHTML( $content );
 			$hfeed_exists = false;
+			
+			
 			//Check each link for rss/atom feeds
 			$website_links = $dom->getElementsByTagName( 'link' );
-
+			
 			if ( $website_links->length > 0 ) {
 				foreach ( $website_links as $link ) {
 					if ( $link->getAttribute( 'rel' ) == 'feed' ) {
@@ -198,7 +212,7 @@ class Yarns_Microsub_Parser {
 					}
 				}
 			}
-
+			
 			// If no h-feed was found as a <link>, then check if the $url itself is an h-feed
 			if ( $hfeed_exists == false ) {
 				if ( static::locate_hfeed( $content, $url ) ) {
@@ -207,13 +221,13 @@ class Yarns_Microsub_Parser {
 					$feeds[]       = $feed;
 				}
 			}
-
+			
 			// return null if no feeds were discovered
-
+			
 			if ( empty( $feeds ) ) {
 				return;
 			}
-
+			
 			// Now that feeds have been discovered, do some clean up and then populate additional fields (author info, photo, description, etc.)
 			foreach ( $feeds as $i => $feed ) {
 				// Convert relative urls to absolute
@@ -221,7 +235,7 @@ class Yarns_Microsub_Parser {
 					$feeds[ $i ]['url'] = $url . $feeds[ $i ]['url'];
 					//$feeds[$i]['url'] = parse_url($url,PHP_URL_SCHEME) + parse_url($url,PHP_URL_HOST) + $feeds[$i]['url'];
 				}
-
+				
 				/*
 				//
 				//  Commenting this out to improve speed
@@ -251,12 +265,12 @@ class Yarns_Microsub_Parser {
 				*/
 				$feeds[ $i ]['type'] = 'feed';
 			}
-
+			
 			return [ 'results' => $feeds ];
 		}
 	}
-
-
+	
+	
 	/**
 	 * Returns a preview of the feed
 	 *
@@ -265,11 +279,11 @@ class Yarns_Microsub_Parser {
 	 * @return array|void
 	 */
 	public static function preview( $url ) {
-
+		
 		return static::parse_feed( $url, 2 );
 		//return Yarns_Microsub_Aggregator::poll_site($url,'_preview');
 	}
-
+	
 	/**
 	 * Parses feed at $url.  Determines whether the feed is h-feed or rss and passes to appropriate
 	 * function.
@@ -280,33 +294,39 @@ class Yarns_Microsub_Parser {
 	 * @return array|void
 	 */
 	public static function parse_feed( $url, $count = 20 ) {
-
+		
 		if ( ! $url ) {
 			return;
 		}
-
-		$content = file_get_contents( $url );
+		
+		$remote = wp_remote_get( $url );
+		
+		if ( is_wp_error( $remote ) ) {
+			return "Error accessing {$url}";
+		}
+		
+		$content = $remote['body'];
 		// only proceed if content could be found
 		if ( ! $content ) {
 			return;
 		}
-
+		
 		// Try to parse h-feed
 		$feed = static::parse_hfeed( $content, $url, $count );
 		if ( $feed ) {
 			return $feed;
 		}
-
+		
 		// If there is no h-feed, Try to parse rss
 		$feed = static::parse_rss( $url, $count );
 		if ( $feed ) {
 			return $feed;
 		}
-
+		
 		// Failed, so return nothing
 		return;
 	}
-
+	
 	/**
 	 * Parse RSS feed at $url
 	 *
@@ -319,28 +339,28 @@ class Yarns_Microsub_Parser {
 		include_once ABSPATH . WPINC . '/feed.php';
 		// Get a SimplePie feed object from the specified feed source.
 		$feed = fetch_feed( $url );
-
+		
 		if ( is_wp_error( $feed ) ) {
 			return;
 		} else {
-
+			
 			$rss_items = [];
-
+			
 			// Parse the feed
 			$feed->enable_cache( false );
 			$feed->strip_htmltags( false );
 			$items = $feed->get_items();
-
+			
 			foreach ( $items as $item ) {
-
+				
 				$post['type'] = 'entry';
 				$post['name'] = htmlspecialchars_decode( $item->get_title(), ENT_QUOTES );
-
+				
 				$post['author']['name'] = $feed->get_title();
-
+				
 				$post['author']['photo'] = $feed->get_image_url();
 				$post['author']['url']   = $feed->get_link();
-
+				
 				// If the individual post has an author with different info from the feed, use that info
 				$post_author = $item->get_author();
 				if ( $post_author->get_name() ) {
@@ -348,35 +368,35 @@ class Yarns_Microsub_Parser {
 						$post['author']['name'] = $post_author->get_name() . ' | ' . $post['author']['name'];
 					}
 				}
-
+				
 				if ( $post_author->get_link() ) {
 					if ( $post_author->get_link() != $post['author']['url'] ) {
 						$post['author']['url'] = $post_author->get_link();
 					}
 				}
-
+				
 				$post['summary'] = strip_tags( $item->get_description() );
-
+				
 				$post['content']['html'] = htmlspecialchars( $item->get_content() );
 				$post['content']['text'] = strip_tags( $item->get_content() );
-
+				
 				//$post['content'] = html_entity_decode ($item->get_content());
 				$post['published'] = gmdate( 'Y-m-d H:i:sO', $item->get_date( 'U' ) );
-
+				
 				$post['url'] = $item->get_permalink();
-
+				
 				$rss_items[] = encode_array( $post ); // encode_array encodes html characters for display
 			}
-
+			
 			return [
 				'items'      => $rss_items,
 				'_feed_type' => 'rss',
 			];
-
+			
 		}
-
+		
 	}
-
+	
 	/*
 
 	$url -> the url from which to retrieve a feed
@@ -385,6 +405,7 @@ class Yarns_Microsub_Parser {
 	*/
 	/**
 	 * Parses h-feed
+	 *
 	 * @param $content
 	 * @param $url
 	 * @param int $count
@@ -392,14 +413,15 @@ class Yarns_Microsub_Parser {
 	 * @return array|void
 	 */
 	public static function parse_hfeed( $content, $url, $count = 5 ) {
-
+		
 		$mf = static::locate_hfeed( $content, $url );
 		//If no h-feed was found, return
 		if ( ! $mf ) {
-			return;}
-
+			return;
+		}
+		
 		//return $mf;
-
+		
 		// Find the key to use
 		if ( ! $mf ) {
 			return ( [
@@ -416,24 +438,25 @@ class Yarns_Microsub_Parser {
 				// If the feed has neither items or chidlren, something has gone wrong
 				return 'No feed items';
 			}
-
+			
 			//return $mf_key;
 		}
 		//error_log("hfeed item key = " . $mf_key );
-
+		
 		// Get feed author
 		// (For posts with no author, use the feed author instead)
 		$feed_author = static::get_feed_author( $content, $url );
-
+		
 		//Get permalinks and contnet for each item
 		$hfeed_items = array();
-
+		
 		foreach ( $mf[ $mf_key ] as $key => $item ) {
 			//error_log ("checkpoint 1.".$key);
 			if ( $key >= $count ) {
-				break;} // Only get up to the specific count of items
+				break;
+			} // Only get up to the specific count of items
 			if ( "{$item['type'][0]}" == 'h-entry' ||
-				"{$item['type'][0]}" == 'h-event' ) {
+			     "{$item['type'][0]}" == 'h-event' ) {
 				$the_item = Parse_This_MF2::parse_hentry( $item, $mf );
 				if ( is_array( $the_item ) ) {
 					/* Merge feed author and post author if:
@@ -453,25 +476,24 @@ class Yarns_Microsub_Parser {
 						} else {
 							// Post author is not set, so replace it with the feed author
 							$the_item['author'] = $feed_author;
+							
 						}
-
-
 					}
-
+					
 					$the_item      = static::clean_post( $the_item );
 					$hfeed_items[] = $the_item;
 				}
 			}
 		}
-
+		
 		//$result = ['items'=> $hfeed_items];
 		return [
 			'items'      => $hfeed_items,
 			'_feed_type' => 'h-feed',
 		];
-
+		
 	}
-
+	
 	/**
 	 * Finds the author of the feed (supplements post author)
 	 *
@@ -482,21 +504,21 @@ class Yarns_Microsub_Parser {
 	 */
 	private static function get_feed_author( $content, $url ) {
 		$mf = Mf2\parse( $content, $url );
-
+		
 		if ( ! is_array( $mf ) ) {
 			return array();
 		}
-
+		
 		$count = count( $mf['items'] );
 		if ( 0 === $count ) {
 			return array();
 		}
 		foreach ( $mf['items'] as $item ) {
 			// Check if the item is an h-card
-
+			
 			if ( Parse_This_MF2::is_type( $item, 'h-card' ) ) {
 				// if (Parse_This_MF2::is_hcard($item)){ // deprecated
-
+				
 				return Parse_This_MF2::parse_hcard( $item, $mf, $url );
 			}
 			// Check if the item is an h-feed, in which case look for an author property
@@ -516,8 +538,8 @@ class Yarns_Microsub_Parser {
 			}
 		}
 	}
-
-
+	
+	
 	/* For now deprecated in favour of mergeparse() */
 	public static function parse_hfeed_item( $content, $url ) {
 		//$mf = Mf2\fetch($url);
@@ -528,42 +550,41 @@ class Yarns_Microsub_Parser {
 				$return_item = array();
 				//$return_item = $item['properties'];
 				$return_item['type'] = $item['type'];
-
+				
 				if ( array_key_exists( 'name', $item['properties'] ) ) {
 					$return_item['name'] = $item['properties']['name'];
 				}
-
+				
 				if ( array_key_exists( 'published', $item['properties'] ) ) {
 					$return_item['published'] = $item['properties']['published'];
 				}
-
+				
 				if ( array_key_exists( 'updated', $item['properties'] ) ) {
 					$return_item['updated'] = $item['properties']['updated'];
 				}
-
+				
 				if ( array_key_exists( 'url', $item['properties'] ) ) {
 					$return_item['url'] = $item['properties']['url'];
 				}
-
+				
 				if ( array_key_exists( 'content', $item['properties'] ) ) {
 					$return_item['content'] = $item['properties']['content'];
 				}
-
+				
 				if ( array_key_exists( 'summary', $item['properties'] ) ) {
 					$return_item['summary'] = $item['properties']['summary'];
 				}
-
+				
 				if ( array_key_exists( 'photo', $item['properties'] ) ) {
 					$return_item['photo'] = $item['properties']['photo'];
 				}
-
+				
 				return $return_item;
 			}
 		}
 	}
-
-
-
+	
+	
 	/**
 	 * Find the root feed for a page
 	 *
@@ -574,18 +595,18 @@ class Yarns_Microsub_Parser {
 	 */
 	public static function locate_hfeed( $content, $url ) {
 		$mf = Mf2\parse( $content, $url );
-
+		
 		if ( ! $mf ) {
 			// If no microformats could be parsed, there is no h-feed
 			return;
 		}
-
+		
 		foreach ( $mf['items'] as $mf_item ) {
 			if ( in_array( 'h-feed', $mf_item['type'] ) ) {
 				return $mf_item;
 			}
 		}
-
+		
 		foreach ( $mf['items'] as $mf_item ) {
 			if ( array_key_exists( 'children', $mf_item ) ) {
 				foreach ( $mf_item['children'] as $child ) {
@@ -615,36 +636,37 @@ class Yarns_Microsub_Parser {
 				}
 			}
 		}
+		
 		return;
 	}
-
+	
 	/** DEPRECATED */
 	public static function find_hfeed_in_page( $url ) {
 		$mf2 = Mf2\fetch( $url );
-
+		
 		// If there was more than one h-entry on the page, treat the whole page as a feed
 		if ( count( $mf2['items'] ) > 1 ) {
 			if ( count(
-				array_filter(
-					$mf2['items'],
-					function( $item ) {
-						return in_array( 'h-entry', $item['type'] );
-					}
-				)
-			) > 1 ) {
+				     array_filter(
+					     $mf2['items'],
+					     function ( $item ) {
+						     return in_array( 'h-entry', $item['type'] );
+					     }
+				     )
+			     ) > 1 ) {
 				#Recognized $url as an h-feed because there are more than one object on the page".
 				// Return the whole page as an hfeed
 				return $mf2;
 			}
 		}
-
+		
 		// If the first item is an h-feed, parse as a feed
 		$first = $mf2['items'][0];
 		if ( in_array( 'h-feed', $first['type'] ) ) {
 			#Parse::debug("mf2:3: Recognized $url as an h-feed because the first item is an h-feed");
 			return $first;
 		}
-
+		
 		// Fallback case, but hopefully we have found something before this point
 		foreach ( $mf2['items'] as $item ) {
 			// Otherwise check for a recognized h-* object
@@ -664,13 +686,13 @@ class Yarns_Microsub_Parser {
 			}
 		}
 	}
-
-
+	
+	
 	/*
 	* UTILITY FUNCTIONS
 	*
 	*/
-
+	
 	/**
 	 * Corrects invalid URLs if possible
 	 *
@@ -683,23 +705,25 @@ class Yarns_Microsub_Parser {
 		if ( static::is_url( $possible_url ) ) {
 			return $possible_url;
 		}
-
+		
 		// If just a word was entered, append .com
 		if ( preg_match( '/^[a-z][a-z0-9]+$/', $possible_url ) ) {
-			  // if just a word was entered, append .com
-			  $possible_url = $possible_url . '.com';
+			// if just a word was entered, append .com
+			$possible_url = $possible_url . '.com';
 		}
-
+		
 		// If the URL is missing a trailing '/' add it
 		//$possible_url = wp_slash($possible_url)
-		if ( substr( $possible_url, -1 ) != '/' ) {
-
+		
+		if ( substr( $possible_url, - 1 ) != '/' ) {
+			
 			$possible_url .= '/';
 		}
+		
 		// If missing a scheme, prepend with 'http://', otherwise return as-is
-		 return parse_url( $possible_url, PHP_URL_SCHEME ) === null ? 'http://' . $possible_url : $possible_url;
+		return parse_url( $possible_url, PHP_URL_SCHEME ) === null ? 'http://' . $possible_url : $possible_url;
 	}
-
+	
 	/**
 	 * Returns true if $query is a valid URL
 	 *
@@ -714,9 +738,10 @@ class Yarns_Microsub_Parser {
 			return false;
 		}
 	}
-
+	
 	/**
 	 * Returns true if $feedtype represents an RSS/Atom feed
+	 *
 	 * @param $feedtype
 	 *
 	 * @return bool
@@ -736,10 +761,6 @@ class Yarns_Microsub_Parser {
 			return true;
 		}
 	}
-
-
-
-
-
-
+	
+	
 }
